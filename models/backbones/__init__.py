6d48afae233bf22923374105c587181de08caf71
mnozkiewcz <michno513@gmail.com>
2024-12-01 03:51:19 +0100

    Added deeplab and res_net backbones

--- models/backbones/__init__.py
+++ models/backbones/__init__.py
@@ -1,1 +1,13 @@
 from .res_net import create_res_net_18, create_res_net_34, create_res_net_50 
+from .backbone import Backbone
+
+
+def init_backbone(backbone: str) -> Backbone:
+    match backbone:
+        case "res_net_18":
+            return create_res_net_18()
+        case "res_net_34":
+            return create_res_net_34()
+        case _:
+            return create_res_net_50()
+        
