7c10bac270eed125a0e463042196559b96128562
Michał Nożkiewicz <mnozkiewicz@id5.io>
2024-12-01 21:16:24 +0100

    Argparse in training method

---
+++ train/argparser.py
@@ -0,0 +1,60 @@
+import argparse
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    available_backbones = [
+        "res_net_50",
+        "res_net_34",
+        "res_net_18",
+        "xception"
+    ]
+
+    parser.add_argument(
+        "--backbone", 
+        type=str, choices=available_backbones, 
+        nargs="?",
+        default="res_net_50",
+        help="Backbone for segmentation net"
+    )
+
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        required=True,
+        help="Path to directory with training data"
+    )
+
+    parser.add_argument(
+        "--patience",
+        type=int,
+        default=20,
+        choices=list(range(3, 51)),
+        metavar="[3-50]",
+        help="Patience in early stopping"
+    )
+
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=16,
+        choices=[16, 32, 64],
+        help="Batch size during data loading"
+    )
+
+    parser.add_argument(
+        "--input_size",
+        type=int,
+        default=None,
+        choices=[128, 256, 512],
+        help="Size of input images (if smaller rescaling is applied)"
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    print(args)
