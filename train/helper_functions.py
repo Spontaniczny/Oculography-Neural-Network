c24adeb99f80b29d0a5df3ece2f8188095be8191
mnozkiewcz <michno513@gmail.com>
2024-12-17 00:56:58 +0100

    Finetuning algorithm

--- train/helper_functions.py
+++ train/helper_functions.py
@@ -6,6 +6,13 @@ from losses.segmentation.losses import DSCLoss, IoULoss, MAE, MCCLoss, FocalLoss
 from losses.regression.losses import WeightedSmoothL1Loss, SineSmoothL1Loss, GaussianLoss
 
 
+def choose_device():
+    if torch.cuda.is_available():
+        return "cuda"
+    if torch.backends.mps.is_available():
+        return "mps"
+    return "cpu"
+
 def get_loss_function(loss_name: str, net_type: str) -> Callable[[torch.Tensor, torch.Tensor], torch.Tensor]:
     if net_type == "segmentation":
         match loss_name:
