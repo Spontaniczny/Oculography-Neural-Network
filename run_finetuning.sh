0f5fcbd9a90538f180dc993321be93c291b7b4e5
mnozkiewcz <michno513@gmail.com>
2024-12-17 14:46:06 +0100

    Changed ellipse net architecture

--- run_finetuning.sh
+++ run_finetuning.sh
@@ -1,8 +1,8 @@
 python -m train.fine_tune \
-    --net_config_file saved_models/deeplab/17:12:2024-00:40:58.json \
+    --net_config_file saved_models/ellipsenet/17:12:2024-14:32:36.json \
     --input_size 256 \
     --dataset "datasets/rat_eye/01|02" \
-    --loss_type dice \
+    --loss_type smooth_l1 \
     --optimizer AdamW \
     --patience 30 \
     --max_epochs 200 \
