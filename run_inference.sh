a046ffc213e587fffcfb7e38e055d8b34fa5408d
mnozkiewcz <michno513@gmail.com>
2025-01-11 23:38:15 +0100

    Clean up

--- run_inference.sh
+++ run_inference.sh
@@ -1,6 +1,6 @@
 python -m inference.run_inference \
-    --dataset_path "datasets/annotated_data/2022_06_01_A_video/frames" \
-    --model_config "saved_models/u_net/03:01:2025-21:44:38.json" \
-    --save_folder "3" \
-    --save_step 1 \
-    --remove_artifacts True \
+    --dataset_path "datasets/extracted_frames/2022_06_01_A_video" \
+    --model_config "saved_models/deeplab/11:01:2025-21:52:47.json" \
+    --save_folder "10" \
+    --save_step 100 \
+    # --remove_artifacts True \
